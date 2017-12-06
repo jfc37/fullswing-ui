68cd7d32e17d46b700a13ba68d79fe3195c998ac
JoeC <placid.joe@gmail.com>
2017-12-06 21:18:28 +1300

    Wait longer after navigation

--- e2e/specs/classes.e2e-spec.ts
+++ e2e/specs/classes.e2e-spec.ts
@@ -4,6 +4,7 @@ import { LoginPage } from '../poms/login.po';
 import { teardown } from '../common/common';
 import { UpdateBlockPage } from '../poms/update-block.po';
 import { ClassListPage } from '../poms/class-list.po';
+import { browser } from 'protractor';
 
 describe('Classes', () => {
   let blockListPage: BlockListPage;
@@ -15,13 +16,13 @@ describe('Classes', () => {
 
     blockListPage.navigateTo();
     new LoginPage().login();
-
-    blockListPage.navigateTo();
   });
 
   afterEach(teardown);
 
   it(`user should be able to browse classes for block`, () => {
+    blockListPage.navigateTo();
+    browser.wait(blockListPage.isOnPage(), null, `Did not navigate to block list page`);
     blockListPage.clickFirstClassListButton();
     expect(classListPage.isOnPage()).toBe(true);
   });
